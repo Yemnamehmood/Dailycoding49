2ead670bcd7ba7506f5ebac456fcd66228757e1b
YemnaMehmood <163342865+Yemnamehmood@users.noreply.github.com>
2024-04-22 23:13:25 +0500

    Dailycodingtask 49
    Callback functions have been used.

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict";
+//DAY 49 
+//TASK 1 (Create a function the accepts a callback and invokes it with some arguments)
+function executecallback(callback, arg1, arg2) {
+    callback(arg1, arg2); //invokes the callback with the specified arguments
+}
+//example callback function that adds two numbers
+let add = (a, b) => {
+    console.log(a + b); //Result the sum of a and b
+};
+executecallback(add, 5, 3); //Result 8
+//invoking a callback function with arguments to perform an addition.
+//TASK 2 (Show an example of a callback function used to filter an array of no)
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//uses .filter() with a callback function to filter out no greater than 5
+let filterdnumbers = numbers.filter((number) => number > 5);
+console.log(filterdnumbers); //Result [6,7,8,9,10]
+//the callback function here checks each no and filter() creates new array with no that meet the criteria
+//TASK 3 (Explain how to handle errors in a callback pattern)
+function fetchdata(callback) {
+    let error = new Error("Failed to fetch data");
+    let data = "Some data";
+    if (Math.random() > 0.5) {
+        callback(null, data); //no error data is fetched successfully
+    }
+    else {
+        callback(error); //passes error to the callback 
+    }
+}
+fetchdata((error, data) => {
+    if (error) {
+        console.error(error.message); //handles the error
+    }
+    else {
+        console.log(data); //process the data 
+    }
+});
+//Demonstrate a callback pattern where errors are handled nicely
